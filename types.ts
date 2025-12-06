21570201e93090d531da84ed60a7bfb2d7728351
guillermo9108 <82621456+guillermo9108@users.noreply.github.com>
2025-12-06 16:51:19 -0500

    Last Sync: 2025-12-06 16:51 (Mobile)

--- types.ts
+++ types.ts
@@ -63,6 +63,7 @@ export interface User {
   watchLater: string[]; 
   sessionToken?: string; 
   avatarUrl?: string; 
+  lastActive?: number;
   defaultPrices?: Record<string, number>; 
   shippingDetails?: {
       fullName: string;
