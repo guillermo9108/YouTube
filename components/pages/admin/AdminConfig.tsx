700a0067a222bdae93a1d2bda65c13a23b6ff2c3
guillermo9108 <82621456+guillermo9108@users.noreply.github.com>
2026-01-03 11:26:57 -0500

    Last Sync: 2026-01-03 11:26 (Mobile)

--- components/pages/admin/AdminConfig.tsx
+++ components/pages/admin/AdminConfig.tsx
@@ -178,7 +178,7 @@ export default function AdminConfig() {
                                             <option value="RANDOM">Aleatorio (Mezclar)</option>
                                         </select>
                                     </div>
-                                </tr>
+                                </div>
                             ))}
                         </div>
                     </div>
